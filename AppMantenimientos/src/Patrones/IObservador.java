a194e87149c621dbb50c4df9cb7b43a4d3bd1c8e
LeandroLagosZ <leandro.lagos@estudiante.ucsm.edu.pe>
2025-11-24 10:44:43 -0500

    Delete AppMantenimientos directory

--- AppMantenimientos/src/Patrones/IObservador.java
+++
@@ -1,5 +0,0 @@
-package Patrones;
-
-public interface IObservador {
-    void notificar(String evento, String mensaje);
-}
--- AppMantenimientos/src/Patrones/LoggerObserver.java
+++
@@ -1,7 +0,0 @@
-package Patrones;
-public class LoggerObserver implements IObservador {
-    @Override
-    public void notificar(String evento, String mensaje) {
-        System.out.println("[LOG - " + evento + "]: " + mensaje);
-    }
-}
